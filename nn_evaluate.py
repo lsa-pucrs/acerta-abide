193248f4f5528ba17622c26717b86d59c39403dc
anibalsolon <anibalsolon@gmail.com>
2017-03-07 14:43:58 -0300

    evaluation parametrization

--- nn_evaluate.py
+++ nn_evaluate.py
@@ -1,20 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+
+Autoencoders evaluation.
+
+Usage:
+  nn_evaluate.py [--folds=N] [--whole] [--male] [--threshold]
+  nn_evaluate.py (-h | --help)
+
+Options:
+  -h --help     Show this screen
+  --folds=N     Number of folds [default: 10]
+  --whole       Run model for the whole dataset
+  --male        Run model for male subjects
+  --threshold   Run model for thresholded subjects
+
+"""
+
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+from docopt import docopt
 from nn import nn, to_softmax, reset
 from utils import format_config
 from sklearn.metrics import confusion_matrix
 
 
-def fold_results(fold, model_path, data_path):
+def fold_results(fold, exp, model_path, data_path):
 
     n_classes = 2
 
-    model_path = format_config(model_path, {'fold': str(fold)})
-    test_path = format_config(data_path, {'fold': str(fold), "datatype": "test"})
+    model_path = format_config(model_path, {"exp": exp, "fold": str(fold)})
+    test_path = format_config(data_path, {"exp": exp, "fold": str(fold), "datatype": "test"})
 
     test_data = np.loadtxt(test_path, delimiter=",")
     test_X, test_y = test_data[:, 1:], test_data[:, 0]
@@ -31,11 +49,11 @@ def fold_results(fold, model_path, data_path):
             saver.restore(sess, model_path)
 
             output = sess.run(
-                model['output'],
+                model["output"],
                 feed_dict={
-                    model['input']: test_X,
-                    model['dropouts'][0]: 1.0,
-                    model['dropouts'][1]: 1.0,
+                    model["input"]: test_X,
+                    model["dropouts"][0]: 1.0,
+                    model["dropouts"][1]: 1.0,
                 }
             )
 
@@ -55,18 +73,27 @@ def fold_results(fold, model_path, data_path):
 
 if __name__ == "__main__":
 
+    arguments = docopt(__doc__)
+
+    experiments = []
+    if arguments["--whole"]:
+        experiments.append("whole")
+    if arguments["--male"]:
+        experiments.append("male")
+    if arguments["--threshold"]:
+        experiments.append("threshold")
+
+    maxfolds = int(arguments["--folds"]) + 1
+
     results = []
+    for exp in experiments:
+        for fold in range(1, maxfolds):
+            results.append([exp, fold] + fold_results(fold, exp, "./data/models/{exp}_{fold}_mlp.ckpt", "./data/corr/{exp}_{fold}_{datatype}.csv"))
 
-    for fold in range(1, 11):
-        results.append(["Whole", fold] + fold_results(fold, "./data/models/mlp-{fold}.ckpt", "./data/corr/corr_1D_cv_{fold}_{datatype}.csv"))
-
-    for fold in range(1, 11):
-        results.append(["Male", fold] + fold_results(fold, "./data/models/mlp-{fold}_male.ckpt", "./data/corr/corr_1D_cv_{fold}_{datatype}_male.csv"))
-
-    cols = ['Exp', 'Fold', 'Accuracy', 'Precision', 'Recall', 'F-score', 'Sensivity', 'Specificity']
+    cols = ["Exp", "Fold", "Accuracy", "Precision", "Recall", "F-score", "Sensivity", "Specificity"]
     df = pd.DataFrame(results, columns=cols)
-    grouped = df.groupby(['Exp'])
+    grouped = df.groupby(["Exp"])
     mean = grouped.agg(np.mean).reset_index()
-    mean['Fold'] = 'Mean'
+    mean["Fold"] = "Mean"
     df = df.append(mean)
-    print df[cols]
+    print df[cols].sort(["Exp", "Fold"])
